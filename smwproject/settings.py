83ac60c125b264f4b5ffad76d920930b9e02b08f
itsluja <lujahawhsuk02@gmail.com>
2021-05-04 22:18:49 +0530

    heroku

--- smwproject/settings.py
+++ smwproject/settings.py
@@ -154,7 +154,7 @@ MEDIA_URL='/media/'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'lujahawhsuk02@gmail.com'
-EMAIL_HOST_PASSWORD = '*123#ajulkushwah'
+EMAIL_HOST_PASSWORD = '#'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
