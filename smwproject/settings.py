b92fd0662af3e4ffb452397b7f3c339c4e4f1808
Ajul-kushwah <ajulkushwah786@gmail.com>
2021-05-05 15:05:49 +0530

    updation

--- smwproject/settings.py
+++ smwproject/settings.py
@@ -137,6 +137,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
